cc967b2125737ad1a11a3ed3f5caf98ce48c426b
Piyush <iampiyushsarkar@gmail.com>
2025-05-10 09:28:06 +0530

    fix: add css to supportedLanguages set

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { workspace } from "vscode";
 
 export const supportedNonCssFiles = new Set(["html", "vue", "svelte", "astro"]);
-export const supportedLanguages = new Set(["scss", "sass", "less", ...supportedNonCssFiles]);
+export const supportedLanguages = new Set(["css", "scss", "sass", "less", ...supportedNonCssFiles]);
 
 export const getConfig = <T>(section: string): T | undefined => {
   return workspace.getConfiguration("sortcss").get(section);
